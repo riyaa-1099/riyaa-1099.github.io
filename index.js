5709f38bbaa84858f2015bd493cb0b23288550e2
riyaa-1099 <112823546+riyaa-1099@users.noreply.github.com>
2023-01-14 20:59:15 +0530

    portfolio completed

--- index.js
+++ index.js
@@ -10,16 +10,45 @@ function typeWriter() {
       i++;
       setTimeout(typeWriter, speed);
     }
+
   }
 
+
   var myNav = document.getElementById('nav-menu');
+  var myNav2 = document.getElementById('main');
   window.onscroll = function () { 
       if ( document.documentElement.scrollTop >= 15 ) {
           myNav.classList.add("nav-colored");
+          myNav2.classList.add("nav-colored");
           // myNav.classList.remove("nav-transparent");
       } 
       else {
           // myNav.classList.add("nav-transparent");
           myNav.classList.remove("nav-colored");
+          myNav2.classList.remove("nav-colored");
       }
   };
+
+  GitHubCalendar(".calendar", "riyaa-1099", {
+    responsive: true,
+    global_stats: false,
+    tooltips: true,
+  });
+
+  document.getElementById("resume-button-1").onclick=()=>{
+window.open("https://drive.google.com/file/d/1MvF6cESiS9amzJ-zqxKn0MJAYQCxfPk-/view?usp=sharing");
+  }
+
+  document.getElementById("resume-button-2").onclick=()=>{
+    window.open("https://drive.google.com/file/d/1MvF6cESiS9amzJ-zqxKn0MJAYQCxfPk-/view?usp=sharing");
+  }
+
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.display= "block";
+  }
